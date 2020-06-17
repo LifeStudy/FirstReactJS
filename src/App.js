89575d3b48ead40ae7be7c057789f3d5e7673921
ArjDev69 <bruno96araujo@gmail.com>
2020-06-17 14:30:55 -0300

    setup eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      T
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>TTT</h1>
+        </div>
+    );
 }
 
 export default App;
