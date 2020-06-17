6b367a0e98b538c344b361a56f7284123f2e2f5f
ArjDev69 <bruno96araujo@gmail.com>
2020-06-17 14:45:11 -0300

    init setup routes

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Repository() {
+    return (
+        <div>
+            <h1>Repository</h1>
+        </div>
+    );
+}
+
+export default Repository;
